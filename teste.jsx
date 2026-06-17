f4b9682711064c34fcdbf8819c2ec26f201b4481
alfredo-amorim <amorimalfred@gmail.com>
2026-06-17 11:49:41 -0400

    Add files via upload

---
+++ teste.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+        fontFamily: "sans-serif",
+        fontSize: "2rem",
+        color: "#333",
+      }}
+    >
+      Em construção
+    </main>
+  );
+}
